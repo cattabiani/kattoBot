5932ca116644a8c75582d2b064bceac2db358a8f
katta <alessandro.cattabiani@gmail.com>
2021-05-25 22:28:57 +0200

    utils -> dice
    black format

--- kattoBot.py
+++ kattoBot.py
@@ -2,7 +2,8 @@ import os
 from dotenv import load_dotenv
 from discord.ext import commands
 import discord
-from src import utils
+from src import dice
+
 
 class KattoBotException(discord.ext.commands.errors.CommandError):
     def __init__(self, msg):
@@ -12,8 +13,8 @@ class KattoBotException(discord.ext.commands.errors.CommandError):
 class KattoBot(commands.Bot):
     def __init__(self):
         load_dotenv("kattobotsandbox.env")
-        self.token = os.getenv('DISCORD_TOKEN')
-        self.guild_name = os.getenv('DISCORD_GUILD')
+        self.token = os.getenv("DISCORD_TOKEN")
+        self.guild_name = os.getenv("DISCORD_GUILD")
 
         intents = discord.Intents.default()
         intents.typing = False
@@ -21,13 +22,13 @@ class KattoBot(commands.Bot):
         intents.members = True
         intents.reactions = False
 
-        super().__init__(intents=intents, command_prefix='!')
+        super().__init__(intents=intents, command_prefix="!")
 
     async def on_ready(self):
         guild = discord.utils.get(self.guilds, name=self.guild_name)
         print(
-            f'{self.user} is connected to the following guild:\n'
-            f'{guild.name}(id: {guild.id})'
+            f"{self.user} is connected to the following guild:\n"
+            f"{guild.name}(id: {guild.id})"
         )
 
     def run(self):
@@ -44,7 +45,9 @@ class KattoBot(commands.Bot):
             return users[0]
         else:
             names = ", ".join([i.name for i in users])
-            raise KattoBotException(f"User name: `{name}` matches too many users: `{names}`")
+            raise KattoBotException(
+                f"User name: `{name}` matches too many users: `{names}`"
+            )
 
     async def send_to_user(self, msg, from0, to0):
         user = self.get_user(to0)
@@ -54,6 +57,7 @@ class KattoBot(commands.Bot):
 
 bot = KattoBot()
 
+
 @bot.event
 async def on_command_error(ctx, error):
     print(type(error))
@@ -62,24 +66,26 @@ async def on_command_error(ctx, error):
         return await ctx.send(str(error))
 
 
-@bot.command(name='r')
+@bot.command(name="r")
 async def roll(ctx, cmd, reply_to=None):
-    res = utils.roll(cmd)
+    res = dice.roll(cmd)
     if not reply_to:
         await ctx.send(res)
     else:
         await ctx.bot.send_to_user(res, ctx.author.name, reply_to)
 
-@bot.command(name='rs')
+
+@bot.command(name="rs")
 async def roll(ctx, cmd, reply_to=None):
-    n, m = cmd.split('d')
+    n, m = cmd.split("d")
     n = int(n)
     m = int(m)
-    res = utils.format_roll_ndm_successes(n, m)
+    res = dice.format_roll_ndm_successes(n, m)
     if not reply_to:
         await ctx.send(res)
     else:
         await ctx.bot.send_to_user(res, ctx.author.name, reply_to)
+
 
 if __name__ == "__main__":
     bot.run()
--- src/dice.py
+++ src/dice.py
@@ -2,6 +2,7 @@ import re
 import random
 import itertools
 
+
 def roll_ndm(n, m, critical_rerolls=False):
     out = []
     i = 0
@@ -11,6 +12,7 @@ def roll_ndm(n, m, critical_rerolls=False):
             i += 1
     return sorted(out, reverse=True)
 
+
 def format_roll_result(v, m):
     if v == 1:
         return "**~~1~~**"
@@ -19,35 +21,42 @@ def format_roll_result(v, m):
     else:
         return str(v)
 
+
 def format_roll_ndm_sum(n, m):
     l = roll_ndm(n, m, False)
-    r_s = ', '.join([format_roll_result(i, m) for i in l])
+    r_s = ", ".join([format_roll_result(i, m) for i in l])
     return f"[{n}d{m}: {r_s}] {sum(l)}"
+
 
 def format_roll_ndm_successes(n, m):
     l = roll_ndm(n, m, True)
 
-    r_s = ', '.join([format_roll_result(i, m) for i in l])
+    r_s = ", ".join([format_roll_result(i, m) for i in l])
     botches = l.count(1)
 
-    freq = [0]*m
+    freq = [0] * m
     for i in l:
-        freq[m-i] += 1
+        freq[m - i] += 1
 
     ps = list(itertools.accumulate(freq))
-    s = [f"vs {i}: {ps[m-i]}" for i in range(m, int(m/2), -1)]
+    s = [f"vs {i}: {ps[m-i]}" for i in range(m, int(m / 2), -1)]
     s = ", ".join(s)
 
     return f"[{n}d{m}: {r_s}]\n**Botches:** {botches}\n**Successes:** {s}"
 
+
 def roll(command):
-    command = re.sub(r"(?<!\[) (?![\w\s]*[\]])","", command)
-    command = re.sub(r"(?<!\[)(\d+)d(\d+)(?![\w\s]*[\]])",lambda m: format_roll_ndm_sum(int(m.group(1)), int(m.group(
-        2))), command)
-    to_compute = re.sub(r'\[[^\[\]]*\]', "", command)
-    command = re.sub(r"(?<!\[)[+-](?![\w\s]*[\]])",lambda m: f" {m.group(0)} ", command)
+    command = re.sub(r"(?<!\[) (?![\w\s]*[\]])", "", command)
+    command = re.sub(
+        r"(?<!\[)(\d+)d(\d+)(?![\w\s]*[\]])",
+        lambda m: format_roll_ndm_sum(int(m.group(1)), int(m.group(2))),
+        command,
+    )
+    to_compute = re.sub(r"\[[^\[\]]*\]", "", command)
+    command = re.sub(
+        r"(?<!\[)[+-](?![\w\s]*[\]])", lambda m: f" {m.group(0)} ", command
+    )
 
     res = eval(to_compute)
 
     return f"{command}\n**Result:** {res}"
-
